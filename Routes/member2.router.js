d91ea96cde83ce52de6ce501e9ddbbfa36eb1f09
Anne Lund <annedeveloper@hotmail.com>
2023-12-10 18:59:10 +0100

    ggsd

--- Routes/member2.router.js
+++ Routes/member2.router.js
@@ -1,23 +1,20 @@
-const express = require('express')
-const {Member2Controller, updateWish, getWishById, deleteWish, getWishes} = require('../Controllers/member2.controller.js')
-const {verifyToken} = require('../Middleware/verifyToken.js')
+const express = require("express");
+const { Member2Controller, updateWish, getWishById, deleteWish, getWishes } = require("../Controllers/member2.controller.js");
+const { verifyToken } = require("../Middleware/verifyToken.js");
 
-const controller = new Member2Controller()
+const controller = new Member2Controller();
 
-const Member2Router = express.Router()
+const Member2Router = express.Router();
 
-Member2Router.get('/member2', getWishes);
+Member2Router.get("/member2", getWishes);
 
-Member2Router.get('/member2/:id', getWishById);
+Member2Router.get("/member2/:id", getWishById);
 
+Member2Router.post("/member2", (req, res) => {
+  controller.create(req, res);
+});
 
-Member2Router.post('/member2', (req, res) => {
-    controller.create(req, res)
-})
+Member2Router.put("/member2/:id", updateWish);
+Member2Router.delete("/member2", deleteWish);
 
-
-Member2Router.put('/member2', updateWish)
-Member2Router.put('/member2/:id', updateWish)
-Member2Router.delete('/member2', deleteWish)
-
-module.exports = {Member2Router}
+module.exports = { Member2Router };
